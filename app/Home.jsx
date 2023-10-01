bff279def063b4d7451d1f6653a3530b0843ad02
mchramaa <rama28moch@gmail.com>
2023-10-01 14:02:31 +0700

    add maskot

--- app/Home.jsx
+++ app/Home.jsx
@@ -2,13 +2,31 @@ import { Text } from "react-native";
 import { View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import Constants from "expo-constants";
+import { Ionicons } from "@expo/vector-icons";
+import Profile from "./Profile";
 
 export default function Home() {
+  let userName = ["User"];
   return (
     <LinearGradient colors={["#00B4D8", "white"]}>
       <View style={{ height: "100%" }}>
         <View style={{ marginTop: Constants.statusBarHeight }}>
-          <Text>Home</Text>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "flex-end",
+            }}
+          >
+            <Ionicons
+              name="ios-person-circle-sharp"
+              size={30}
+              color="white"
+              onPress={Profile}
+            />
+          </View>
+          <View style={{}}>
+            <Text>Hallo {userName}</Text>
+          </View>
         </View>
       </View>
     </LinearGradient>
---
+++ app/Profile.jsx
@@ -0,0 +1,16 @@
+import { Text } from "react-native";
+import { View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import Constants from "expo-constants";
+
+export default function Profile() {
+  return (
+    <LinearGradient colors={["#00B4D8", "white"]}>
+      <View style={{ height: "100%" }}>
+        <View style={{ marginTop: Constants.statusBarHeight }}>
+          <Text>Profile</Text>
+        </View>
+      </View>
+    </LinearGradient>
+  );
+}
